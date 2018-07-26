1a0b58d2c4a4f31e336ea3397487fa982f042902
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2018-07-26 15:26:20 +0300

    Make channel updates use aec_id:id()-s

--- apps/aehttp/test/aehttp_integration_SUITE.erl
+++ apps/aehttp/test/aehttp_integration_SUITE.erl
@@ -3599,8 +3599,8 @@ channel_update(#{initiator := IConnPid, responder :=RConnPid},
     %% verify contents
     {channel_offchain_tx, OffchainTx} = aetx:specialize_type(UnsignedStateTx),
     [Update] = aesc_offchain_tx:updates(OffchainTx),
-    Expected = aesc_offchain_update:op_transfer(StarterPubkey,
-                                                AcknowledgerPubkey, Amount),
+    Expected = aesc_offchain_update:op_transfer(aec_id:create(account, StarterPubkey),
+                                                aec_id:create(account, AcknowledgerPubkey), Amount),
     Expected = Update,
 
 
